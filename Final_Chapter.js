b7469cd1da1367385959576d6b6b7af5471bddb2
Naxe21 <146705989+Naxe21@users.noreply.github.com>
2025-07-09 13:07:50 +0700

    Delete Final_Chapter.js

--- Final_Chapter.js
+++
@@ -1,7 +0,0 @@
-function closePopup() {
-  const overlay = document.getElementById("overlay");
-  overlay.classList.add("fade-out");
-
-  setTimeout(() => {
-    overlay.style.display = "none";
-  }, 500); }
